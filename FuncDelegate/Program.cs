72f27861c24b0fdb1e5e852e073952b18911f479
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-15 22:39:35 -0300

    Utilizando a função lambda diretamente dentro do método Select da classe List (inline)

--- FuncDelegate/Program.cs
+++ FuncDelegate/Program.cs
@@ -14,11 +14,9 @@ namespace FuncDelegate
             list.Add(new Product("Tv", 900.00));
             list.Add(new Product("Mouse", 50.00));
             list.Add(new Product("Tablet", 350.50));
-            list.Add(new Product("HD Case", 80.90));
+            list.Add(new Product("HD Case", 80.90));                        
 
-            Func<Product, string> func = p => p.Name.ToUpper();
-
-            List<string> result = list.Select(func).ToList();
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
             foreach (string s in result)
             {
                 Console.WriteLine(s);
